2c12d8b03d3df0d52316f46eb72a1856b691e392
cometang <1403029829@qq.com>
2019-11-19 22:06:13 +0800

    前端页面持续集成

---
+++ src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+          <ul>
+            <li v-for="(item,index) in users" :key="index">
+              {{item.name}}
+            </li>
+          </ul>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'home',
+ data(){
+   return{
+     users:[]
+   }
+ },
+ methods:{
+
+ },
+ mounted(){
+   axios.get('http://localhost:3000/api/users')
+   .then(res=>{
+     console.log(res)
+     this.users = res.data
+   })
+ }
+}
+</script>
